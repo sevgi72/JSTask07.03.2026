64772157ee91aff6550c3826739d0a13a66b4240
sevgi72 <sevgimf@code.edu.az>
2026-03-13 22:37:19 +0400

    basket

---
+++ js/script3.js
@@ -0,0 +1,59 @@
+let buttons = document.querySelectorAll(".btn-primary");
+
+buttons.forEach(function(button){
+
+    button.addEventListener("click", function(ev){
+
+        ev.preventDefault();
+
+        let parent = this.parentNode;
+
+        let id = parent.getAttribute("data-id");
+        let name = parent.querySelector(".card-title").innerText;
+        let desc = parent.querySelector(".desc").innerText;
+
+        let priceText = parent.querySelector(".price").innerText;
+        let price = +priceText.substring(priceText.indexOf(":")+1, priceText.indexOf("$"));
+
+        let url = parent.previousElementSibling.getAttribute("src");
+
+        let basketStr = localStorage.getItem("basket");
+        let products = [];
+
+        if(basketStr){
+            products = JSON.parse(basketStr);
+        }
+
+        let existproduct = products.find(p => p.id == id);
+
+        if(existproduct){
+            existproduct.count++;
+        }
+        else{
+            let product = {
+                id:id,
+                name:name,
+                desc:desc,
+                price:price,
+                url:url,
+                count:1
+            };
+
+            products.push(product);
+        }
+
+        localStorage.setItem("basket", JSON.stringify(products));
+
+        BasketCount();
+
+    });
+
+});
+// function BasketCount(){
+//       let basketStr = localStorage.getItem("basket");
+//       if(basketStr){
+//         let products=JSON.parse(basketStr);
+//         let basketCount=document.getElementById("basketCount");
+//         basketCount.innerText=products.length;
+//       }
+// }
